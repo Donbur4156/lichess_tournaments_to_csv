a825c5ef4fef960cddfc49eda98f1e2eaff25af2
jplight <janpfister@t-online.de>
2021-01-31 02:56:11 +0100

    V2.0
    Options for the output are implemented

--- tournamenttool_swiss.py
+++ tournamenttool_swiss.py
@@ -13,6 +13,13 @@ parser.read("parameter_swiss.ini")
 configObject = parser["PARAMS"]
 id_arena = configObject["arenaID"]
 file_name = configObject["output_file_name"]
+rank_bool = configObject["rank"]
+username_bool = configObject["username"]
+score_bool = configObject["score"]
+tieBreak_bool = configObject["tieBreak"]
+rating_bool = configObject["rating"]
+performance_bool = configObject["performance"]
+
 
 #download arena data of lichess
 url = "https://lichess.org/api/swiss/" + id_arena + "/results"
@@ -22,17 +29,46 @@ list_resp = resp.text.splitlines()
 data = list(map(lambda x: json.loads(x), list_resp))
 
 #create CSV file
-columns = ["rank", "username", "score", "tieBreak", "rating", "performance"]
+columns = []
+
+if rank_bool == "true":
+    columns.append("rank")
+if username_bool == "true":
+    columns.append("username")
+if score_bool == "true":
+    columns.append("score")
+if tieBreak_bool == "true":
+    columns.append("tieBreak")
+if rating_bool == "true":
+    columns.append("rating")
+if performance_bool == "true":
+    columns.append("performance")
+
+
+print(columns)
 now = datetime.datetime.today()
 time = now.strftime('%Y%m%d_%H%M')
 with open(file_name + time + '.csv', 'w', newline='') as file:
     writer = csv.writer(file)
     writer.writerow(columns)
     for i in data:
-        rank = i.get("rank") + 1
-        username = i.get("username")
-        score = i.get("score")
-        tieBreak = i.get("tieBreak")
-        rating = i.get("rating")
-        performance = i.get("performance")
-        writer.writerow([rank, username, score, tieBreak, rating, performance])
+        column = []
+        if rank_bool == "true":
+            rank = i.get("rank") + 1
+            column.append(rank)
+        if username_bool == "true":
+            username = i.get("username")
+            column.append(username)
+        if score_bool == "true":
+            score = i.get("score")//1000000/10
+            column.append(score)
+        if tieBreak_bool == "true":
+            tieBreak = i.get("tieBreak")
+            column.append(tieBreak)
+        if rating_bool == "true":
+            rating = i.get("rating")
+            column.append(rating)
+        if performance_bool == "true":
+            performance = i.get("performance")
+            column.append(performance)
+        writer.writerow(column)
